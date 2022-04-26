9a96669be8bb9ded53f5a34494b0c5a490933fa9
iaasmo <smokussc@gmail.com>
2022-04-26 18:46:26 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <WeatherView />
+</template>
+
+<script>
+import WeatherView from './components/WeatherView.vue'
+
+export default {
+  name: 'WeatherView',
+  components: {
+    WeatherView,
+    data(){
+      return{
+        weather: "sun",
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/WeatherView.vue
@@ -0,0 +1,23 @@
+<template>
+  <WeatherView />
+</template>
+
+<script>
+import WeatherView from './components/WeatherView.vue'
+export default {
+  name: 'App',
+  components: {
+    WeatherView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
